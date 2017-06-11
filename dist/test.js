ad094966bcbff92ee249a362bf05ee4f4afdaa44
blackmiaool <blackmiaool@gmail.com>
2017-06-11 15:22:12 +0800

    fix loader

--- dist/test.js
+++ dist/test.js
@@ -73,138 +73,138 @@
 "use strict";
 
 
-const _a = 'uowl#-*jGgrvLtVpy.adWcMmETensxhi';
-const _b = 'imdyv#.lxtMu-jVsWerEocwh*aGgnTpL';
-const _c = 'Vjs-g.LuoEGraMpmxvewty#cTWl*ndhi';
-const _d = 'TyvjdrnwWEGVoxcMuagit-p.#*mhelsL';
-const _e = 'e.M-xEjn#usWgw*mpoVyGdrvcTLliaht';
-const _f = 'cVxsGM.nreilWjwphv*TaogLtym-#dEu';
-const _g = 'jr*.x#yTmiEuVclhLgGavptedM-snwWo';
-const _h = 'LhdugVtMjws#rmGoe.-EyWvi*xcnlTap';
-const _i = 'gVumaTowiv.-rLMyxnel#hEGdpctsj*W';
-const _j = 'ynmeLxol*MTEdpawGr#ch-iWgtjVvus.';
-var inputWindow = $(_g[5] + _b[0] + _g[28] + _f[15] + _b[11] + _j[25])[0][_g[13] + _a[1] + _f[7] + _b[9] + _i[18] + _h[27] + _f[24] + _j[23] + _j[22] + _a[27] + _f[29] + _a[1] + _f[14]];
-var outputWindow = $(_e[8] + _i[6] + _j[29] + _c[20] + _i[25] + _f[31] + _i[27])[0][_a[21] + _j[6] + _e[7] + _a[13] + _j[3] + _f[7] + _d[20] + _h[21] + _f[10] + _j[1] + _i[24] + _c[8] + _a[2]];
-var $errorOutput = $(_c[22] + _e[0] + _e[22] + _d[5] + _b[20] + _c[11] + _j[21] + _d[12] + _j[29] + _h[6] + _j[13] + _a[0] + _j[25]);
+const _a = 'ciEauWgxveL*MpdmGnjwyot#TsVlh.r-';
+const _b = 'neirovuVctG.plhjwdaMEx#sLy*WgT-m';
+const _c = 'emojhGvu#wW-.MxrLn*pdiEsgayTcVlt';
+const _d = 'exdp*syLlTigEhGw-MtWncVr#.uavomj';
+const _e = 'wmtVWen-ulvTLg*yprxd.acijM#EhsGo';
+const _f = 'omchdnx.ueyLVtv*gMwipaErjTGls#W-';
+const _g = 'Tix.vte*wELgyusG#odnpmlVahjW-Mcr';
+const _h = '.rEeuxMcvwWjhnoagt*iGlTVym#-Lsdp';
+const _i = 'dTGso.vEl*tjMhgnVperyiLWu-mc#xaw';
+const _j = 'GwuLV*vr#psgyWhoMaT-xm.einEdtljc';
+var inputWindow = $(_d[24] + _a[1] + _h[13] + _b[12] + _f[8] + _g[5])[0][_d[21] + _g[17] + _h[13] + _h[17] + _a[9] + _i[15] + _d[18] + _g[27] + _b[2] + _f[5] + _f[4] + _i[4] + _i[31]];
+var outputWindow = $(_b[22] + _f[0] + _b[6] + _i[10] + _g[20] + _g[13] + _f[13])[0][_b[8] + _c[2] + _c[17] + _j[28] + _c[0] + _g[19] + _f[13] + _d[19] + _i[21] + _a[17] + _j[27] + _e[31] + _i[31]];
+var $errorOutput = $(_i[28] + _h[3] + _d[23] + _g[31] + _c[2] + _i[19] + _a[31] + _b[4] + _b[6] + _i[10] + _c[19] + _b[6] + _g[5]);
 var setOutputReady = void 0;
 var outputReady = new Promise(function (resolve, reject) {
-    const _a2 = _f;
-    const _b2 = _h;
-    const _c2 = _j;
-    const _d2 = _d;
-    const _e2 = _i;
-    const _f2 = _c;
+    const _a2 = _h;
+    const _b2 = _c;
+    const _c2 = _i;
+    const _d2 = _f;
+    const _e2 = _g;
+    const _f2 = _b;
     const _g2 = _a;
-    const _h2 = _e;
-    const _i2 = _b;
-    const _j2 = _g;
+    const _h2 = _j;
+    const _i2 = _e;
+    const _j2 = _d;
 
     setOutputReady = resolve;
 });
 var demoReady = new Promise(function (resolve, reject) {
-    const _a3 = _a;
-    const _b3 = _j;
-    const _c3 = _d;
-    const _d3 = _i;
-    const _e3 = _h;
+    const _a3 = _d;
+    const _b3 = _a;
+    const _c3 = _i;
+    const _d3 = _g;
+    const _e3 = _f;
     const _f3 = _e;
-    const _g3 = _c;
-    const _h3 = _b;
-    const _i3 = _f;
-    const _j3 = _g;
+    const _g3 = _b;
+    const _h3 = _c;
+    const _i3 = _h;
+    const _j3 = _j;
 
-    $[_g[19] + _j[26] + _a[18] + _f[2]]({
-        [_j[29] + _e[22] + _j[7]]: _i[24] + _h[16] + _i[3] + _g[31] + _j[31] + _d[3] + _e[10],
-        [_b[9] + _d[1] + _j[13] + _b[17]]: _g[18] + _a[24] + _a[25],
-        [_g[24] + _e[29] + _i[27] + _b[25] + _c[24] + _h[20] + _c[14] + _d[28]]: _i[27] + _a[26] + _j[5] + _g[22],
-        [_d[30] + _g[11] + _g[13] + _c[23] + _a[26] + _h[10] + _b[15]]: resolve
+    $[_c[25] + _c[3] + _e[21] + _a[7]]({
+        [_b[6] + _d[23] + _f[27]]: _e[19] + _f[9] + _h[25] + _f[0] + _b[11] + _b[15] + _g[14],
+        [_a[22] + _j[12] + _j[9] + _a[9]]: _d[14] + _h[2] + _h[22],
+        [_h[30] + _g[24] + _a[22] + _g[24] + _g[0] + _h[24] + _c[19] + _e[5]]: _h[17] + _d[0] + _i[29] + _c[31],
+        [_g[14] + _g[13] + _d[21] + _b[8] + _i[18] + _a[25] + _d[5]]: resolve
     });
 });
-window[_e[29] + _e[21] + _b[2] + _j[11] + _b[4] + _h[16] + _a[27] + _g[22] + _b[31] + _e[28] + _d[30] + _d[20] + _j[3] + _h[27] + _g[23] + _h[12]](_g[8] + _d[28] + _f[3] + _j[30] + _e[29] + _f[22] + _j[3], function (_ref) {
-    const _a4 = _h;
-    const _b4 = _g;
-    const _c4 = _j;
-    const _d4 = _c;
-    const _e4 = _b;
+window[_g[24] + _f[4] + _c[20] + _c[22] + _f[14] + _a[9] + _f[5] + _h[17] + _f[11] + _b[2] + _d[5] + _j[28] + _h[3] + _j[25] + _e[5] + _a[30]](_d[30] + _i[18] + _g[14] + _j[10] + _j[17] + _d[11] + _b[1], function (_ref) {
+    const _a4 = _d;
+    const _b4 = _b;
+    const _c4 = _a;
+    const _d4 = _f;
+    const _e4 = _e;
     const _f4 = _i;
-    const _g4 = _a;
-    const _h4 = _f;
-    const _i4 = _e;
-    const _j4 = _d;
+    const _g4 = _g;
+    const _h4 = _j;
+    const _i4 = _c;
+    const _j4 = _h;
 
-    var source = _ref[_e[10] + _e[17] + _a[0] + _g[1] + _i[26] + _f[9]],
-        _ref$data = _ref[_b[2] + _c[12] + _j[25] + _h[30]],
-        action = _ref$data[_e[29] + _i[26] + _d[20] + _e[28] + _e[17] + _c[28]],
-        data = _ref$data[_h[2] + _d[17] + _j[25] + _h[30]];
+    var source = _ref[_e[29] + _g[17] + _f[8] + _j[7] + _f[2] + _j[23]],
+        _ref$data = _ref[_j[27] + _c[25] + _g[5] + _j[17]],
+        action = _ref$data[_g[24] + _d[21] + _b[9] + _f[19] + _h[14] + _f[5]],
+        data = _ref$data[_d[2] + _b[18] + _b[9] + _b[18]];
 
     switch (action) {
-        case _c[11] + _h[16] + _a[18] + _e[21] + _h[20]:
+        case _g[31] + _d[0] + _g[24] + _g[18] + _f[10]:
             if (source === outputWindow) {
                 setOutputReady();
             } else if (source === inputWindow) {
-                demoReady[_h[6] + _d[27] + _j[3] + _f[7]](function (demoCode) {
-                    const _a5 = _d;
-                    const _b5 = _i;
-                    const _c5 = _j;
-                    const _d5 = _e;
-                    const _e5 = _c;
-                    const _f5 = _h;
-                    const _g5 = _g;
-                    const _h5 = _b;
-                    const _i5 = _f;
-                    const _j5 = _a;
+                demoReady[_j[28] + _g[25] + _c[0] + _a[17]](function (demoCode) {
+                    const _a5 = _h;
+                    const _b5 = _a;
+                    const _c5 = _b;
+                    const _d5 = _i;
+                    const _e5 = _f;
+                    const _f5 = _e;
+                    const _g5 = _j;
+                    const _h5 = _c;
+                    const _i5 = _d;
+                    const _j5 = _g;
 
-                    inputWindow[_i[25] + _h[15] + _j[30] + _e[31] + _h[7] + _f[9] + _a[28] + _a[28] + _i[4] + _h[4] + _a[26]]({
-                        [_e[29] + _j[19] + _b[9] + _a[31] + _c[8] + _i[17]]: _h[10] + _j[3] + _b[9] + _i[1] + _h[30] + _e[27] + _h[3] + _e[0],
-                        [_c[29] + _c[12] + _i[27] + _h[30]]: demoCode
-                    }, _g[2]);
+                    inputWindow[_c[19] + _d[29] + _e[29] + _f[13] + _b[19] + _h[3] + _i[3] + _a[25] + _a[3] + _j[11] + _j[23]]({
+                        [_a[3] + _b[8] + _f[13] + _g[1] + _f[0] + _c[17]]: _c[23] + _g[6] + _c[31] + _c[29] + _i[30] + _i[8] + _g[13] + _a[9],
+                        [_a[14] + _c[25] + _g[5] + _g[24]]: demoCode
+                    }, _i[9]);
                 });
             }
             break;
-        case _d[14] + _g[15] + _f[20] + _i[17] + _c[4] + _j[3]:
+        case _b[8] + _f[3] + _i[30] + _e[6] + _f[16] + _b[1]:
             if (source === inputWindow) {
                 var code = void 0;
-                $errorOutput[_d[28] + _a[23] + _f[15] + _e[31] + _g[6]]();
+                $errorOutput[_h[3] + _d[30] + _g[20] + _i[10] + _c[26]]();
                 try {
                     code = decentMessUp(data);
                 } catch (e) {
-                    $errorOutput[_g[22] + _e[0] + _d[13] + _g[22]](e[_h[13] + _h[16] + _i[28] + _c[2] + _e[29] + _b[27] + _b[17]]);
-                    outputReady[_c[20] + _c[30] + _e[0] + _g[28]](function () {
-                        const _a6 = _d;
-                        const _b6 = _h;
-                        const _c6 = _c;
-                        const _d6 = _g;
-                        const _e6 = _f;
-                        const _f6 = _b;
-                        const _g6 = _a;
-                        const _h6 = _j;
-                        const _i6 = _e;
-                        const _j6 = _i;
+                    $errorOutput[_c[31] + _a[9] + _i[29] + _i[10]](e[_j[21] + _a[9] + _e[29] + _d[5] + _i[30] + _d[11] + _d[0]]);
+                    outputReady[_b[9] + _h[12] + _i[18] + _c[17]](function () {
+                        const _a6 = _f;
+                        const _b6 = _e;
+                        const _c6 = _g;
+                        const _d6 = _c;
+                        const _e6 = _a;
+                        const _f6 = _j;
+                        const _g6 = _d;
+                        const _h6 = _i;
+                        const _i6 = _h;
+                        const _j6 = _b;
 
-                        outputWindow[_i[25] + _g[31] + _i[28] + _i[27] + _b[10] + _h[16] + _c[2] + _d[30] + _b[25] + _e[12] + _j[3]]({
-                            [_j[14] + _i[26] + _j[25] + _d[19] + _h[15] + _a[27]]: _f[3] + _j[3] + _b[9] + _j[27] + _i[4] + _f[11] + _g[11] + _e[0],
-                            [_c[29] + _i[4] + _c[20] + _e[29]]: ""
-                        }, _f[18]);
+                        outputWindow[_e[16] + _j[15] + _j[10] + _h[17] + _d[17] + _f[9] + _h[29] + _g[14] + _b[18] + _d[11] + _b[1]]({
+                            [_d[27] + _c[28] + _e[2] + _e[23] + _g[17] + _c[17]]: _a[25] + _h[3] + _i[10] + _g[23] + _j[17] + _f[27] + _c[7] + _h[3],
+                            [_f[4] + _j[17] + _a[22] + _b[18]]: ""
+                        }, _i[9]);
                     });
                     return;
                 }
 
-                outputReady[_g[22] + _c[30] + _f[9] + _i[17]](function () {
-                    const _a7 = _i;
-                    const _b7 = _b;
-                    const _c7 = _j;
-                    const _d7 = _c;
-                    const _e7 = _a;
-                    const _f7 = _d;
-                    const _g7 = _h;
-                    const _h7 = _g;
-                    const _i7 = _e;
-                    const _j7 = _f;
+                outputReady[_d[18] + _i[13] + _e[5] + _c[17]](function () {
+                    const _a7 = _d;
+                    const _b7 = _a;
+                    const _c7 = _e;
+                    const _d7 = _j;
+                    const _e7 = _b;
+                    const _f7 = _i;
+                    const _g7 = _g;
+                    const _h7 = _f;
+                    const _i7 = _h;
+                    const _j7 = _c;
 
-                    outputWindow[_d[22] + _h[15] + _g[27] + _a[13] + _f[5] + _f[9] + _b[15] + _h[10] + _j[14] + _d[18] + _d[28]]({
-                        [_g[19] + _c[23] + _g[22] + _e[28] + _a[1] + _h[27]]: _j[30] + _i[18] + _j[25] + _h[5] + _h[30] + _g[14] + _g[11] + _d[28],
-                        [_h[2] + _j[14] + _j[25] + _a[18]]: code
-                    }, _c[27]);
+                    outputWindow[_b[12] + _j[15] + _h[29] + _f[13] + _c[13] + _f[9] + _d[5] + _i[3] + _g[24] + _d[11] + _a[9]]({
+                        [_c[25] + _e[22] + _f[13] + _a[1] + _b[4] + _c[17]]: _d[5] + _h[3] + _j[28] + _d[22] + _a[3] + _f[27] + _h[4] + _f[9],
+                        [_b[17] + _b[18] + _e[2] + _c[25]]: code
+                    }, _c[18]);
                 });
             }
             break;
--- loader.js
+++ loader.js
@@ -1,6 +1,4 @@
 const messup=require("./index.js");
 module.exports = function(source) {
-    console.log(123)
-    
   return messup(source,this.query);
 };
